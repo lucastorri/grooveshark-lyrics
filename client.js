4d210d4c0fcbe6b0093fae1fa91d238d2972e1b8
Ivayr Netto <netto@netto-Latitude-E6510.(none)>
2011-07-14 17:48:41 -0300

    adding client-side tests

--- client.js
+++ client.js
@@ -2,3 +2,16 @@ var musicData = $('.active').last().children('a[class^="queueSong"]');
 var song = musicData[0].text;
 var artist = musicData[1].text;
 console.log(song, artist);
+
+$(function(){
+	$.ajax({
+	    url: 'http://lyrics-service.herokuapp.com/?music=Hotel&artist=Eagles',
+	    dataType: 'jsonp',
+	    crossDomain: true,
+	    jsonp: false,
+	    jsonpCallback: 'result',
+	    success: function(data) {
+	      $('#lyrics').html(data.lyrics);
+	    }
+	});
+});
