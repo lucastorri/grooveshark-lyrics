ca310d2eb59e8932c1ec4084685587bdf328e527
Netto <nettofarah@gmail.com>
2011-07-15 15:48:12 -0300

    getting style from .css

--- client.js
+++ client.js
@@ -7,19 +7,16 @@ var songInfo = function(){
 var info = songInfo();
 
 var adjustStyles = (function(){
-	$('#capital').css('width', '280px');
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.css" />');
 	$('body').resize(function(){
 		$('#application').css('width', $('body').width() - $('#capital').width());	
 	});
 	$('#application').css('width', $('body').width() - $('#capital').width()); 
 	$('#remove_capital_button').remove();
-	$('#mainContainer').css('background', 'none');
-	$('#capital_header').css('text-align', 'center');
 	$('#capitalPane').css('height', $('#main').height() - 50);
 	$('#main').resize(function() {
   		$('#capitalPane').css('height', $('#main').height() - 50);
 	});
-	$('#capitalPane').css('overflow', 'auto');
 })();
 
 setInterval(function(){
@@ -33,10 +30,7 @@ setInterval(function(){
 	    	jsonpCallback: 'result',
 	    	success: function(data) {
 	      		$('#capitalPane').html(data.lyrics);
-	      		$('#capitalPane p').css('margin', '10px');
 	      		$('#capital_header').html('<p>'+ data.artist + '</p><p><b>' + data.music + '</b></p>');
-	      		$('#capital_header > p').css('text-align', 'center');
-	      		$('#capital_header p:first').css('padding-top', '5px');
 	    	}
 		});	
 	}
