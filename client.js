148dc5692041c6e87a22acf5241ca1345f9c52b0
Netto <nettofarah@gmail.com>
2011-07-14 22:04:53 -0300

    improving UI

--- client.js
+++ client.js
@@ -1,16 +1,31 @@
-var song = Grooveshark._lastStatus.activeSong.SongName;
-var artist = Grooveshark._lastStatus.activeSong.ArtistName;
+var songInfo = function(){
+	return{
+		music: Grooveshark._lastStatus.activeSong.SongName,
+		artist: Grooveshark._lastStatus.activeSong.ArtistName
+	}; 
+};
 
-$(function(){
-	$.ajax({
-	    url: 'http://lyrics-service.herokuapp.com/?music='+encodeURIComponent(song)+'&artist='+encodeURIComponent(artist),
-	    dataType: 'jsonp',
-	    crossDomain: true,
-	    jsonp: false,
-	    jsonpCallback: 'result',
-	    success: function(data) {
-	      console.log(data);
-	      $('#capital').html(data.lyrics);
-	    }
-	});
+$('#capital p').css('margin', '10px');
+
+var info = songInfo();
+
+setInterval(function(){
+	var currentSong = songInfo();
+	if(currentSong != info){
+		$.ajax({
+	    	url: 'http://lyrics-service.herokuapp.com/?music='+encodeURIComponent(currentSong.music)+'&artist='+encodeURIComponent(currentSong.artist),
+	    	dataType: 'jsonp',
+	    	crossDomain: true,
+	    	jsonp: false,
+	    	jsonpCallback: 'result',
+	    	success: function(data) {
+	      		$('#capital').html(data.lyrics);
+	    	}
+		});	
+	}
+}, 5000);
+
+
+$.getScript("ajax/test.js", function() {
+  alert("Load was performed.");
 });
