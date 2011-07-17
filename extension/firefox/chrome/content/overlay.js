06c60605d622fadd0d6d580b5e82bae8427104ca
Duda Dornelles <dudassdornelles@gmail.com>
2011-07-16 22:25:17 -0300

    when clicking in the addon, go to grooveshark and run the script on document:load

--- extension/firefox/chrome/content/overlay.js
+++ extension/firefox/chrome/content/overlay.js
@@ -1,11 +1,20 @@
 var gsl = {
   start: function() {
     var gslScript = 
-      "var gslScript = document.createElement('script');"
-      + "gslScript.setAttribute('type','text/javascript');"
-      + "gslScript.setAttribute('charset','UTF-8');"
-      + "gslScript.setAttribute('src','https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js');"
-      + "document.body.appendChild(gslScript);";
+      "var execIt = function () {"
+      +   "var gslScript = document.createElement('script');"
+      +   "gslScript.setAttribute('type','text/javascript');"
+      +   "gslScript.setAttribute('charset','UTF-8');"
+      +   "gslScript.setAttribute('src','https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js');"
+      +   "document.body.appendChild(gslScript);";
+      + "}"
+      + "if (/grooveshark\.com/.exec(document.location.href) == null) {"
+      +   "document.location='http://grooveshark.com';"
+      +   "window.addListener('execIt', execIt, false);"
+      + "}"
+      + "else {"
+      +   "execIt();"
+      + "}";
 
     var safewin = new XPCNativeWrapper(window); 
     var sandbox = new Components.utils.Sandbox(safewin); 
